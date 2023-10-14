67db17f0288f1b74ca4c70e8fa04551e2b8dad4c
fecrakib <rakibislam132575@gmail.com>
2023-10-14 09:53:16 +0600

    nav-bar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#E02C6D",
+        footer:"#0A0826",
+      
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        inter:['Inter Tight', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
